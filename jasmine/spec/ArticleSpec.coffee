1d389c46a095d9947ed12dc62cda361c35761426
Lincoln Lee <git@lincoln.hk>
2013-02-04 21:30:27 +0800

    Update test with whitespace around input which fit real world more

--- jasmine/spec/ArticleSpec.coffee
+++ jasmine/spec/ArticleSpec.coffee
@@ -2,11 +2,11 @@ describe "AppleDaily", ->
   article = title = content = date = section = view_count = html = null
   beforeEach ->
     article = new AppleDaily()
-    title = "Test title"
+    title = "   Test title     "
     content = "<p>Test Content 1</p><p>Test Content 2</p><h2>Sub Title</h2><p>Test Content 3</p>"
-    date = "2013年01月31日"
+    date = "  2013年01月31日  "
     section = "Test section"
-    view_count = "1,234"
+    view_count = "  1,234   "
     html = """
     <html>
       <head></head>
@@ -15,7 +15,7 @@ describe "AppleDaily", ->
           <div class="LHS">
             <div class="LHSTitle page_news">
               <div class="LHSTitle_inner">
-                <div class="SelectHdate">2013年01月31日</div>
+                <div class="SelectHdate">   2013年01月31日   </div>
               </div>
             </div>
             <div id="articleContent" class="LHSContent">
@@ -24,10 +24,10 @@ describe "AppleDaily", ->
                   <tbody>
                     <tr>
                       <td>
-                        <h1>Test title</h1>
+                        <h1>   Test title    </h1>
                       </td>
                       <td>
-                        <div class="view">1,234</div>
+                        <div class="view">   1,234   </div>
                       </td>
                     </tr>
                   </tbody>
@@ -60,7 +60,7 @@ describe "AppleDaily", ->
 
   it "should be able to set title", ->
     article.set_title title
-    expect(article.title).toEqual title
+    expect(article.title).toEqual "Test title"
 
   it "should be able to set content", ->
     article.set_content content
@@ -68,7 +68,7 @@ describe "AppleDaily", ->
 
   it "should be able to set date", ->
     article.set_date date
-    expect(article.date).toEqual date
+    expect(article.date).toEqual "2013年01月31日"
 
   it "should be able to set section", ->
     article.set_section section
@@ -76,7 +76,7 @@ describe "AppleDaily", ->
 
   it "should be able to set view count", ->
     article.set_view_count view_count
-    expect(article.view_count).toEqual view_count
+    expect(article.view_count).toEqual "1,234"
 
   describe "Parser", ->
     article = null
@@ -84,10 +84,10 @@ describe "AppleDaily", ->
       article = new AppleDaily()
       article.parse html
     it "should set title", ->
-      expect(article.title).toEqual title
+      expect(article.title).toEqual "Test title"
     it "should set content", ->
       expect(article.content).toEqual content
     it "should set view count", ->
-      expect(article.view_count).toEqual view_count
+      expect(article.view_count).toEqual "1,234"
     it "should set date", ->
-      expect(article.date).toEqual date
+      expect(article.date).toEqual "2013年01月31日"
--- jasmine/spec/ArticleSpec.js
+++ jasmine/spec/ArticleSpec.js
@@ -6,16 +6,16 @@
     article = title = content = date = section = view_count = html = null;
     beforeEach(function() {
       article = new AppleDaily();
-      title = "Test title";
+      title = "   Test title     ";
       content = "<p>Test Content 1</p><p>Test Content 2</p><h2>Sub Title</h2><p>Test Content 3</p>";
-      date = "2013年01月31日";
+      date = "  2013年01月31日  ";
       section = "Test section";
-      view_count = "1,234";
-      return html = "<html>\n  <head></head>\n  <body>\n    <div class=\"container\">\n      <div class=\"LHS\">\n        <div class=\"LHSTitle page_news\">\n          <div class=\"LHSTitle_inner\">\n            <div class=\"SelectHdate\">2013年01月31日</div>\n          </div>\n        </div>\n        <div id=\"articleContent\" class=\"LHSContent\">\n          <div class=\"LHSBorderBox\">\n            <table class=\"LinkTable\">\n              <tbody>\n                <tr>\n                  <td>\n                    <h1>Test title</h1>\n                  </td>\n                  <td>\n                    <div class=\"view\">1,234</div>\n                  </td>\n                </tr>\n              </tbody>\n            </table>\n          </div>\n          <div id=\"masterContent\" class=\"fontSize2\">\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <p class=\"ArticleIntro\">Test Content 1</p>\n              </div>\n            </div>\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <p class=\"ArticleIntro\">Test Content 2</p>\n              </div>\n            </div>\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <h2>Sub Title</h2>\n                <p class=\"ArticleIntro\">Test Content 3</p>\n              </div>\n            </div>\n          </div>\n        </div>\n      </div>\n    </div>\n  </body>\n</html>";
+      view_count = "  1,234   ";
+      return html = "<html>\n  <head></head>\n  <body>\n    <div class=\"container\">\n      <div class=\"LHS\">\n        <div class=\"LHSTitle page_news\">\n          <div class=\"LHSTitle_inner\">\n            <div class=\"SelectHdate\">   2013年01月31日   </div>\n          </div>\n        </div>\n        <div id=\"articleContent\" class=\"LHSContent\">\n          <div class=\"LHSBorderBox\">\n            <table class=\"LinkTable\">\n              <tbody>\n                <tr>\n                  <td>\n                    <h1>   Test title    </h1>\n                  </td>\n                  <td>\n                    <div class=\"view\">   1,234   </div>\n                  </td>\n                </tr>\n              </tbody>\n            </table>\n          </div>\n          <div id=\"masterContent\" class=\"fontSize2\">\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <p class=\"ArticleIntro\">Test Content 1</p>\n              </div>\n            </div>\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <p class=\"ArticleIntro\">Test Content 2</p>\n              </div>\n            </div>\n            <div class=\"ArticleContent_Outer\">\n              <div class=\"ArticleContent_Inner\">\n                <h2>Sub Title</h2>\n                <p class=\"ArticleIntro\">Test Content 3</p>\n              </div>\n            </div>\n          </div>\n        </div>\n      </div>\n    </div>\n  </body>\n</html>";
     });
     it("should be able to set title", function() {
       article.set_title(title);
-      return expect(article.title).toEqual(title);
+      return expect(article.title).toEqual("Test title");
     });
     it("should be able to set content", function() {
       article.set_content(content);
@@ -23,7 +23,7 @@
     });
     it("should be able to set date", function() {
       article.set_date(date);
-      return expect(article.date).toEqual(date);
+      return expect(article.date).toEqual("2013年01月31日");
     });
     it("should be able to set section", function() {
       article.set_section(section);
@@ -31,7 +31,7 @@
     });
     it("should be able to set view count", function() {
       article.set_view_count(view_count);
-      return expect(article.view_count).toEqual(view_count);
+      return expect(article.view_count).toEqual("1,234");
     });
     return describe("Parser", function() {
       article = null;
@@ -40,16 +40,16 @@
         return article.parse(html);
       });
       it("should set title", function() {
-        return expect(article.title).toEqual(title);
+        return expect(article.title).toEqual("Test title");
       });
       it("should set content", function() {
         return expect(article.content).toEqual(content);
       });
       it("should set view count", function() {
-        return expect(article.view_count).toEqual(view_count);
+        return expect(article.view_count).toEqual("1,234");
       });
       return it("should set date", function() {
-        return expect(article.date).toEqual(date);
+        return expect(article.date).toEqual("2013年01月31日");
       });
     });
   });
